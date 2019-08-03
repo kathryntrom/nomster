e3fe92b49d85af69db0a7530bb89b9296e54008b
Kathryn Tromba <kathryntromba@gmail.com>
2019-08-03 18:05:44 +0000

    saving lesson 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@stpetehangs.com"
 
   def comment_added
-    mail(to: "kathryntromba@gmail.com",
+    mail(to: "whenuru@gmail.com",
          subject: "A comment has been added to your place")
   end
 end
